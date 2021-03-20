540c6bdf87ebd0219581df30c1c8cd7008b8072f
d0nat <szy.szymalski@gmail.com>
2021-03-20 18:49:05 +0100

    Dodanie "link" do "ProductModel"

--- Skapiec_APP/SqliteDataAccess.cs
+++ Skapiec_APP/SqliteDataAccess.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using System.Configuration;
 using System.Data.SQLite;
@@ -24,6 +21,7 @@ namespace Skapiec_APP
         public string image { get; set; }
         public string title { get; set; }
         public string price { get; set; }
+        public string link { get; set; }
     }
 
 
